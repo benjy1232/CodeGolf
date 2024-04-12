d35ae7ee97f8ae049da0ee92a3983d115f1ae108
Ben Serrano <b.serrano8970@gmail.com>
2024-04-12 10:56:36 -0500

    Kill gofmt

--- dotproduct/main.go
+++ dotproduct/main.go
@@ -4,20 +4,4 @@ import (
 	"fmt"
 	"os"
 )
-func main(){
-	var ub []byte
-	var vb []byte
-	var u []int
-	var v []int
-	fmt.Scanln(&ub)
-	fmt.Scanln(&vb)
-	e:=json.Unmarshal(ub,&u)
-	if e!=nil{fmt.Println("error: ",e)}
-	e=json.Unmarshal(vb,&v)
-	if e!=nil{fmt.Println("error: ",e)}
-	vs:=len(u)
-	if vs!=len(v){fmt.Println("Vectors are different sizes");os.Exit(1)}
-	s:=0
-	for i:=0;i<vs;i++{s=s+u[i]*v[i]}
-	fmt.Println(s)
-}
+func main(){var U []byte;var V []byte;var u []int;var v []int;fmt.Scanln(&U);fmt.Scanln(&V);e:=json.Unmarshal(U,&u);if e!=nil{fmt.Println("error: ",e)};e=json.Unmarshal(V,&v);if e!=nil{fmt.Println("error: ",e)};l:=len(u);if l!=len(v){fmt.Println("Vectors are different sizes");os.Exit(1)};s:=0;for i:=0;i<l;i++{s=s+u[i]*v[i]};fmt.Println(s)}
