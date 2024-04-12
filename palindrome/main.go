a12a8df055b3aac7f3a90449116c3e2b7df12f28
Ben Serrano <b.serrano8970@gmail.com>
2024-04-12 10:51:59 -0500

    RIP Go standards

--- palindrome/main.go
+++ palindrome/main.go
@@ -3,21 +3,4 @@ import (
 	"fmt"
 	"os"
 )
-func main() {
-	var s string
-	fmt.Scanln(&s)
-	sl:=len(s)
-	if sl == 0 {
-		os.Exit(2)
-	}
-	cl:=sl / 2
-	if sl%2==1 {
-		cl--
-	}
-	for i:=0; i<cl; i++ {
-		if s[i]!=s[sl-i-1] {
-			os.Exit(1)
-		}
-	}
-	os.Exit(0)
-}
+func main(){var s string;fmt.Scanln(&s);sl:=len(s);if sl==0{os.Exit(2)};cl:=sl/2;if sl%2==1{cl--};for i:=0;i<cl;i++{if s[i]!=s[sl-i-1]{os.Exit(1)}};os.Exit(0)}
