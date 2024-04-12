8c93445ac447ac597a9565e3c3abbedfde5c46ab
Ben Serrano <b.serrano8970@gmail.com>
2024-04-12 10:08:05 -0500

    Condensing

--- palindrome/main.go
+++ palindrome/main.go
@@ -6,20 +6,18 @@ import (
 )
 
 func main() {
-	var str string
-	fmt.Scan(&str)
-	strLength := len(str)
-	if strLength == 0 {
+	var s string
+	fmt.Scanln(&s)
+	sl:=len(s)
+	if sl == 0 {
 		os.Exit(2)
 	}
-
-	compareLength := strLength / 2
-	if strLength % 2 == 1 {
-		compareLength--
+	cl:=sl / 2
+	if sl%2==1 {
+		cl--
 	}
-
-	for i := 0; i < compareLength; i++ {
-		if str[i] != str[strLength - i - 1] {
+	for i:=0; i<cl; i++ {
+		if s[i]!=s[sl-i-1] {
 			os.Exit(1)
 		}
 	}
